7666d9f34c51085edb38e23a806ff0b2c236d2c3
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2018-01-12 09:46:06 +0100

    FileManager class added.

---
+++ MessageFromSpace/FileManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MessageFromSpace
+{
+    static class FileManager
+    {
+        public static string GetFileContent()
+        {
+            string name = ReadFileName();
+            string fileContent = ReadFile(name);
+
+            return fileContent;
+        }
+
+        private static string ReadFileName()
+        {
+            string name;
+            Console.WriteLine("Podaj nazwę pliku: ");
+            do
+            {
+                name = Console.ReadLine();
+            } while (String.IsNullOrEmpty(name) && IsFileNameCorrect(name));
+
+            return name;
+        }
+
+        private static Boolean IsFileNameCorrect(string name)
+        {
+            if(name.ToCharArray()[0] == '3' && name.ToCharArray()[1] == '0')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static string ReadFile(string name)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(name))
+                {
+                    String fileContent = sr.ReadToEnd();
+                    return fileContent;
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Nie znaleziono pliku o podanej nazwie");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Nie można otworzyć pliku");
+            }
+
+            return "";
+        }
+    }
+}
--- MessageFromSpace/Program.cs
+++ MessageFromSpace/Program.cs
@@ -7,6 +7,7 @@ namespace MessageFromSpace
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            FileManager.ReadFileName();
         }
     }
 }
